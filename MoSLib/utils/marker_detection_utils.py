fb5a04b40e28656d696edf16e9a5a21553c89dda
Emre Iper <emreiper789@hotmail.com>
2023-08-26 22:15:14 +0300

    Aruco marker detection test

---
+++ MoSLib/utils/marker_detection_utils.py
@@ -0,0 +1,27 @@
+import cv2
+import cv2.aruco as aruco 
+
+video_capture = False
+cap = cv2.VideoCapture(1)
+
+def find_aruco(img, marker_size=4, total_markers=250, draw=True):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    key = getattr(aruco,f'DICT_{marker_size}X{marker_size}_{total_markers}')
+    arucoDict = aruco.getPredefinedDictionary(key)
+    arucoParam = aruco.DetectorParameters()
+    bbox, ids, _= aruco.detectMarkers(gray, dictionary=arucoDict, parameters=arucoParam)
+    print(ids)
+    if draw:
+        aruco.drawDetectedMarkers(img, bbox)
+
+    return bbox, ids
+
+while True:
+    if video_capture: _, img = cap.read()
+    else: 
+        img = cv2.imread("Images/aruco.jpg")
+        img = cv2.resize(img, (0,0), fx=0.2, fy=0.2)
+    bbox, ids = find_aruco(img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    cv2.imshow("img", img)
