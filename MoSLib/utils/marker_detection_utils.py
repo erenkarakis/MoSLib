e3e7fe56c75a1f9a8fdb2ad09d10cbe7fe57463e
Emre Iper <emreiper789@hotmail.com>
2023-09-06 14:00:39 +0300

    Inside of image folder deleted

--- MoSLib/utils/marker_detection_utils.py
+++ MoSLib/utils/marker_detection_utils.py
@@ -4,7 +4,7 @@ import cv2.aruco as aruco
 video_capture = False
 cap = cv2.VideoCapture(1)
 
-def find_aruco(img, marker_size=4, total_markers=250, draw=True):
+def find_aruco(img, marker_size=7, total_markers=250, draw=True):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     key = getattr(aruco,f'DICT_{marker_size}X{marker_size}_{total_markers}')
     arucoDict = aruco.getPredefinedDictionary(key)
@@ -19,7 +19,7 @@ def find_aruco(img, marker_size=4, total_markers=250, draw=True):
 while True:
     if video_capture: _, img = cap.read()
     else: 
-        img = cv2.imread("Images/aruco.jpg")
+        img = cv2.imread("Images/aruco.png")
         img = cv2.resize(img, (0,0), fx=0.2, fy=0.2)
     bbox, ids = find_aruco(img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
