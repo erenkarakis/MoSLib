b6d085ff2a835ab066927d12867083ffd6745479
Emre Iper <emreiper789@hotmail.com>
2023-09-13 12:57:21 +0300

    Marker detection update

--- MoSLib/utils/marker_detection_utils.py
+++ MoSLib/utils/marker_detection_utils.py
@@ -1,27 +1,44 @@
+import numpy as np
 import cv2
-import cv2.aruco as aruco 
+import cv2.aruco as aruco
 
-video_capture = False
-cap = cv2.VideoCapture(1)
+aruco_size = 5 # For 5X5 aruco detection.
+aruco_dict_param = f"aruco.DICT_{aruco_size}X{aruco_size}_250" # If you need fewer markers, use a smaller dictionary (50, 100, 250, 1000)
 
-def find_aruco(img, marker_size=7, total_markers=250, draw=True):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    key = getattr(aruco,f'DICT_{marker_size}X{marker_size}_{total_markers}')
-    arucoDict = aruco.getPredefinedDictionary(key)
-    arucoParam = aruco.DetectorParameters()
-    bbox, ids, _= aruco.detectMarkers(gray, dictionary=arucoDict, parameters=arucoParam)
-    print(ids)
-    if draw:
-        aruco.drawDetectedMarkers(img, bbox)
+cap = cv2.VideoCapture(0) # Get camera source
 
-    return bbox, ids
-
-while True:
-    if video_capture: _, img = cap.read()
-    else: 
-        img = cv2.imread("Images/aruco.png")
-        img = cv2.resize(img, (0,0), fx=0.2, fy=0.2)
-    bbox, ids = find_aruco(img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    cv2.imshow("img", img)
+def track(matrix_coefficients, distortion_coefficients):
+    """ 
+    ### PARAMETERS
+    `matrix_coefficients` : Calibration matrix from the camera calibration process.\n
+    `distortion_coefficients` : Distortion coefficients from the camera calibration process.
+    """
+    while True:
+        ret, frame = cap.read()
+        # operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Change grayscale
+        aruco_dict = aruco.Dictionary_get(aruco_dict_param)  # Use (arcuco_size)X(aruco_size) dictionary to find markers
+        parameters = aruco.DetectorParameters_create()  # Marker detection parameters
+        # lists of ids and the corners beloning to each id
+        corners, ids, rejected_img_points = aruco.detectMarkers(gray, aruco_dict,
+                                                                parameters=parameters,
+                                                                cameraMatrix=matrix_coefficients,
+                                                                distCoeff=distortion_coefficients)
+        if np.all(ids is not None):  # If there are markers found by detector
+            for i in range(0, len(ids)):  # Iterate in markers
+                # Estimate pose of each marker and return the values rvec and tvec---different from camera coefficients
+                rvec, tvec, markerPoints = aruco.estimatePoseSingleMarkers(corners[i], 0.02, matrix_coefficients,
+                                                                           distortion_coefficients)
+                (rvec - tvec).any()  # get rid of that nasty numpy value array error
+                aruco.drawDetectedMarkers(frame, corners)  # Draw A square around the markers
+                aruco.drawAxis(frame, matrix_coefficients, distortion_coefficients, rvec, tvec, 0.01)  # Draw Axis
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        # Wait 3 milisecoonds for an interaction. Check the key and do the corresponding job.
+        key = cv2.waitKey(3) & 0xFF
+        if key == ord('q'):  # Quit
+            break
+    
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
