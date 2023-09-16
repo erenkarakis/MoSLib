ec0412d3e17f0c3cd0399ec9951289cc597c7333
Taha Eren Karakış <jpantherr@gmail.com>
2023-09-16 12:26:12 +0300

    MoSLib core update

--- MoSLib/MoSLib.py
+++ MoSLib/MoSLib.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from numpy import ndarray
 from math import tan, radians
 from .utils import math_utils
@@ -89,9 +90,6 @@ def triangulate_streo(self, left_cam: tuple[float, float], right_cam: tuple[floa
     """
     pass
 
-def find_disparity(rcam_pt: tuple[int, int], lcam_pt: tuple[int, int]):
-    return lcam_pt[0] - rcam_pt[0]
-
 def perspective_projection(rcam_pt: tuple[int, int], lcam_pt: tuple[int, int], home_point: tuple[int, int], fl: float, c2c_distance: float, disparity: int):
 
     if disparity == 0 or disparity == None:
@@ -103,4 +101,44 @@ def perspective_projection(rcam_pt: tuple[int, int], lcam_pt: tuple[int, int], h
     z = (c2c_distance * fl) / disparity
 
     return x, y, z
+
+def depth_map(imgL, imgR):
+    """ Depth map calculation. Works with SGBM and WLS. Need rectified images, returns depth map ( left to right disparity ) """
+    # SGBM Parameters -----------------
+    window_size = 3  # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
+
+    left_matcher = cv2.StereoSGBM_create(
+        minDisparity=-1,
+        numDisparities=5*16,  # max_disp has to be dividable by 16 f. E. HH 192, 256
+        blockSize=window_size,
+        P1=8 * 3 * window_size,
+        # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
+        P2=32 * 3 * window_size,
+        disp12MaxDiff=12,
+        uniquenessRatio=10,
+        speckleWindowSize=50,
+        speckleRange=32,
+        preFilterCap=63,
+        mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY
+    )
+    right_matcher = cv2.ximgproc.createRightMatcher(left_matcher)
+    # FILTER Parameters
+    lmbda = 80000
+    sigma = 1.3
+    visual_multiplier = 6
+
+    wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=left_matcher)
+    wls_filter.setLambda(lmbda)
+
+    wls_filter.setSigmaColor(sigma)
+    displ = left_matcher.compute(imgL, imgR)  # .astype(np.float32)/16
+    dispr = right_matcher.compute(imgR, imgL)  # .astype(np.float32)/16
+    displ = np.int16(displ)
+    dispr = np.int16(dispr)
+    filteredImg = wls_filter.filter(displ, imgL, None, dispr)  # important to put "imgL" here!!!
+
+    filteredImg = cv2.normalize(src=filteredImg, dst=filteredImg, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX);
+    filteredImg = np.uint8(filteredImg)
+
+    return filteredImg
 
