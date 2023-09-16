ed1784a26b9b46463b8d3f32ff43378db598be32
Taha Eren Karakış <jpantherr@gmail.com>
2023-09-16 13:06:49 +0300

    Test code update

--- example2.py
+++ example2.py
@@ -2,6 +2,74 @@ import numpy as np
 import cv2
 import argparse
 import sys
+import MoSLib
+
+
+def save_coefficients(mtx, dist, path):
+    """ Save the camera matrix and the distortion coefficients to given path/file. """
+    cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_WRITE)
+    cv_file.write("K", mtx)
+    cv_file.write("D", dist)
+    # note you *release* you don't close() a FileStorage object
+    cv_file.release()
+
+
+def save_stereo_coefficients(path, K1, D1, K2, D2, R, T, E, F, R1, R2, P1, P2, Q):
+    """ Save the stereo coefficients to given path/file. """
+    cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_WRITE)
+    cv_file.write("K1", K1)
+    cv_file.write("D1", D1)
+    cv_file.write("K2", K2)
+    cv_file.write("D2", D2)
+    cv_file.write("R", R)
+    cv_file.write("T", T)
+    cv_file.write("E", E)
+    cv_file.write("F", F)
+    cv_file.write("R1", R1)
+    cv_file.write("R2", R2)
+    cv_file.write("P1", P1)
+    cv_file.write("P2", P2)
+    cv_file.write("Q", Q)
+    cv_file.release()
+
+
+def load_coefficients(path):
+    """ Loads camera matrix and distortion coefficients. """
+    # FILE_STORAGE_READ
+    cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_READ)
+
+    # note we also have to specify the type to retrieve other wise we only get a
+    # FileNode object back instead of a matrix
+    camera_matrix = cv_file.getNode("K").mat()
+    dist_matrix = cv_file.getNode("D").mat()
+
+    cv_file.release()
+    return [camera_matrix, dist_matrix]
+
+
+def load_stereo_coefficients(path):
+    """ Loads stereo matrix coefficients. """
+    # FILE_STORAGE_READ
+    cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_READ)
+
+    # note we also have to specify the type to retrieve other wise we only get a
+    # FileNode object back instead of a matrix
+    K1 = cv_file.getNode("K1").mat()
+    D1 = cv_file.getNode("D1").mat()
+    K2 = cv_file.getNode("K2").mat()
+    D2 = cv_file.getNode("D2").mat()
+    R = cv_file.getNode("R").mat()
+    T = cv_file.getNode("T").mat()
+    E = cv_file.getNode("E").mat()
+    F = cv_file.getNode("F").mat()
+    R1 = cv_file.getNode("R1").mat()
+    R2 = cv_file.getNode("R2").mat()
+    P1 = cv_file.getNode("P1").mat()
+    P2 = cv_file.getNode("P2").mat()
+    Q = cv_file.getNode("Q").mat()
+
+    cv_file.release()
+    return [K1, D1, K2, D2, R, T, E, F, R1, R2, P1, P2, Q]
 
 parser = argparse.ArgumentParser(description='Camera calibration')
 parser.add_argument('--calibration_file', type=str, required=True, help='Path to the stereo calibration file')
@@ -52,7 +120,7 @@ while True:  # Loop until 'q' pressed or stream ends
     gray_left = cv2.cvtColor(left_rectified, cv2.COLOR_BGR2GRAY)
     gray_right = cv2.cvtColor(right_rectified, cv2.COLOR_BGR2GRAY)
 
-    disparity_image = depth_map(gray_left, gray_right)  # Get the disparity map
+    disparity_image = MoSLib.depth_map(gray_left, gray_right)  # Get the disparity map
 
     # Show the images
     cv2.imshow('left(R)', leftFrame)
