582e81c21668a58d61d31629c4480f8d5358ef00
Taha Eren Karakış <jpantherr@gmail.com>
2023-09-16 12:24:56 +0300

    Testing codes updated

--- example.py
+++ example.py
@@ -22,10 +22,11 @@ previousTime = 0
 while True:
     cTime = time.time()
 
-    success1, frame_lcam = lcam.read()
-    success2, frame_rcam = rcam.read()
+    success1, frame_rcam = lcam.read()
+    success2, frame_lcam = rcam.read()
 
-    frame_lcam = cv2.flip(frame_lcam, 0)
+    # frame_rcam = cv2.flip(frame_rcam, 0)
+    # frame_rcam = cv2.flip(frame_rcam, 1)
 
     frame_lcam = cv2.cvtColor(frame_lcam, cv2.COLOR_BGR2GRAY)
     frame_rcam = cv2.cvtColor(frame_rcam, cv2.COLOR_BGR2GRAY)
@@ -38,13 +39,13 @@ while True:
             x, y, z = MoSLib.perspective_projection(match[0], match[1], (640, 360), 1530.0, c2c_distance, disparity)
         except:
             print("Cannot unpack non-iterable NoneType object")
-        print(match[1], z)
+        print(match[0], z)
 
     MoSLib.visual_utils.fps_counter(frame_lcam, cTime, previousTime)
     previousTime = cTime
 
-    cv2.imshow("c1", frame_lcam)
-    cv2.imshow("c2", frame_rcam)
+    cv2.imshow("Left", frame_lcam)
+    cv2.imshow("Right", frame_rcam)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
